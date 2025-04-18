0e2b801c5c48a1d3a60ab9e66562da08030290d2
Maxine <maxine@mxhzl.com>
2025-04-17 21:08:38 -0400

    add volume

--- terraform/droplets.tf
+++ terraform/droplets.tf
@@ -37,3 +37,15 @@ resource "digitalocean_reserved_ip" "mxhzl" {
   droplet_id = digitalocean_droplet.mxhzl.id
   region     = digitalocean_droplet.mxhzl.region
 }
+
+resource "digitalocean_volume" "mxhzl" {
+  region                  = digitalocean_droplet.mxhzl.region
+  name                    = "mxhzl"
+  size                    = 10
+  initial_filesystem_type = "ext4"
+}
+
+resource "digitalocean_volume_attachment" "mxhzl" {
+  droplet_id = digitalocean_droplet.mxhzl.id
+  volume_id  = digitalocean_volume.mxhzl.id
+}
