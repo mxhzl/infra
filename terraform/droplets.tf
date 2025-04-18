310a17fe2e3bc346579541a70bae9f84f3cbe6f4
Maxine <maxine@mxhzl.com>
2025-04-17 21:44:00 -0400

    firewall

--- terraform/droplets.tf
+++ terraform/droplets.tf
@@ -7,6 +7,12 @@ resource "digitalocean_droplet" "mxhzl" {
   name   = "mxhzl"
   region = "nyc1"
   size   = "s-1vcpu-1gb"
+  backups = true
+  backup_policy {
+    plan    = "weekly"
+    weekday = "SUN"
+    hour    = 20
+  }
   ssh_keys = [
     data.digitalocean_ssh_key.ssh_key.id
   ]
@@ -48,4 +54,45 @@ resource "digitalocean_volume" "mxhzl" {
 resource "digitalocean_volume_attachment" "mxhzl" {
   droplet_id = digitalocean_droplet.mxhzl.id
   volume_id  = digitalocean_volume.mxhzl.id
+}
+
+resource "digitalocean_firewall" "mxhzl" {
+  name = "inbound-22-80-443-only"
+
+  droplet_ids = [digitalocean_droplet.mxhzl.id]
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "22"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "80"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "443"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "tcp"
+    port_range            = "1-65535"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "udp"
+    port_range            = "1-65535"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "icmp"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
 }
