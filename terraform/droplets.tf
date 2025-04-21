c1179fb37fcb15964e9cd414b8fd84b217b0260f
Maxine <maxine@mxhzl.com>
2025-04-21 19:05:46 -0400

    get linkding running via docker compose

--- terraform/droplets.tf
+++ terraform/droplets.tf
@@ -25,18 +25,6 @@ resource "digitalocean_droplet" "mxhzl" {
     agent_identity = "Rook"
   }
 
-  provisioner "remote-exec" {
-    inline = [
-      "export PATH=$PATH:/usr/bin",
-      # install caddy
-      "sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https curl",
-      "curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg",
-      "curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list",
-      "sudo apt update",
-      "sudo apt install -y caddy"
-    ]
-  }
-
 }
 
 resource "digitalocean_reserved_ip" "mxhzl" {
